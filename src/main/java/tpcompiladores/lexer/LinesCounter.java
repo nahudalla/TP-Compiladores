3496c7b67477c83b4ad29985cf123e9d2b90be5a
Lucas Olivera <oliveralucasm@gmail.com>
2019-09-08 14:00:51 -0300

    I've created the class LinesCounter wich function is to count every time it receives an endline character

---
+++ src/main/java/tpcompiladores/lexer/LinesCounter.java
@@ -0,0 +1,15 @@
+package tpcompiladores.lexer;
+
+public class LinesCounter implements CharactersObserver {
+    private long numberOfLines;
+
+    public long getCurrentLineNumber(){
+        return this.numberOfLines;
+    }
+
+    @Override
+    public void processReadCharacter(Character readCharacter) {
+        if(readCharacter.equals('\n'))
+            numberOfLines += 1;
+    }
+}
