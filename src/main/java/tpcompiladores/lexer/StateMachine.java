a9172bc3784510094d66da4885990c479722ac6e
Lucas Olivera <oliveralucasm@gmail.com>
2019-09-10 15:49:39 -0300

    StateMachine and StateTransition classes

--- src/main/java/tpcompiladores/lexer/StateMachine.java
+++ src/main/java/tpcompiladores/lexer/StateMachine.java
@@ -1,6 +1,35 @@
 package tpcompiladores.lexer;
 
 
-public class StateMachine {
+import java.util.ArrayList;
+import java.util.List;
 
+public class StateMachine {
+    private int currentState = 0;
+    private StateTransition[][] transitionMatrix;
+    private List<CharacterFilter> characterClasses = new ArrayList<>();
+
+    public StateMachine (StateTransition[][] transitionMatrix, List characterClasses) {
+        this.transitionMatrix = transitionMatrix;
+        this.characterClasses = characterClasses;
+    }
+
+    public int getCurrentState() {
+        return this.currentState;
+    }
+
+    public void performTransition(Character readCharacter, LexerContext lexerContext) {
+        boolean matchedClass = false;
+        int characterClass = -1;
+
+        for(int i = 0; i < this.characterClasses.size() && !matchedClass; i++) {
+            if (this.characterClasses.get(i).matches(readCharacter)){
+                matchedClass = true;
+                characterClass = i;
+            }
+        }
+
+        this.transitionMatrix[this.currentState][characterClass].getSemanticAction().run(lexerContext);
+        this.currentState = this.transitionMatrix[this.currentState][characterClass].getNextState();
+    }
 }
--- src/main/java/tpcompiladores/lexer/StateTransition.java
+++ src/main/java/tpcompiladores/lexer/StateTransition.java
@@ -1,5 +1,19 @@
 package tpcompiladores.lexer;
 
 public class StateTransition {
+    private int nextState = 0;
+    private SemanticAction semanticAction;
 
+    public StateTransition(int nextState, SemanticAction semanticAction) {
+        this.nextState = nextState;
+        this.semanticAction = semanticAction;
+    }
+
+    public int getNextState() {
+        return nextState;
+    }
+
+    public SemanticAction getSemanticAction() {
+        return semanticAction;
+    }
 }
