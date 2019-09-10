863623bb3be6f7a34402d7b1e0c4faceadd6b9be
Agustin Gerez <agustingerez2@gmail.com>
2019-09-10 16:06:51 -0300

    Class Lexer

--- src/main/java/tpcompiladores/lexer/Lexer.java
+++ src/main/java/tpcompiladores/lexer/Lexer.java
@@ -1,5 +1,21 @@
 package tpcompiladores.lexer;
 
 public class Lexer {
+    private int nextToken;
 
+    public Lexer(int nextToken) {
+        this.nextToken = nextToken;
+    }
+
+    public int getNextToken() {
+        return nextToken;
+    }
+
+    public void setNextToken(int nextToken) {
+        this.nextToken = nextToken;
+    }
+
+    public void setNextToken(int nextToken, String keyToken){
+        this.nextToken = nextToken;
+    }
 }
