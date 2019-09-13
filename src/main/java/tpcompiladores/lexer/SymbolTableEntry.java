33de68f366f81812005d42fd64e70f99c8be647f
Agustin Gerez <agustingerez2@gmail.com>
2019-09-13 15:06:09 -0300

    Class modificated and reviewed

--- src/main/java/tpcompiladores/lexer/SymbolTableEntry.java
+++ src/main/java/tpcompiladores/lexer/SymbolTableEntry.java
@@ -1,19 +1,13 @@
 package tpcompiladores.lexer;
 
-import java.util.Map;
-
 public class SymbolTableEntry {
     private String lexeme;
-
-    public SymbolTableEntry(String lexeme) {
-        this.lexeme = lexeme;
-    }
 
     public String getLexeme() {
         return this.lexeme;
     }
 
-    public void setLexema(String lexeme) {
+    public void setLexeme(String lexeme) {
         this.lexeme = lexeme;
     }
 }
--- src/main/java/tpcompiladores/lexer/semanticActions/EmitIdentifierOrReservedWord.java
+++ src/main/java/tpcompiladores/lexer/semanticActions/EmitIdentifierOrReservedWord.java
@@ -3,33 +3,55 @@ package tpcompiladores.lexer.semanticActions;
 import tpcompiladores.lexer.LexerContext;
 import tpcompiladores.lexer.SemanticAction;
 import tpcompiladores.lexer.SymbolTableEntry;
+import tpcompiladores.lexer.TokenNumbers;
 
 public class EmitIdentifierOrReservedWord implements SemanticAction {
     private String word;
-
-    public EmitIdentifierOrReservedWord(String word){
-        this.word = word;
-    }
+    private LexerContext lexerContext;
 
     @Override
     public void run(LexerContext lexerContext) {
         this.word = lexerContext.getCharactersRecorder().getRecordedString();
+        this.lexerContext = lexerContext;
 
-       if (lexerContext.getReservedWordsTable().containsKey(this.word)) {
-           Integer value = lexerContext.getReservedWordsTable().get(this.word);
-           lexerContext.getLexer().setNextToken(value,this.word);
-       }
-        if  (!lexerContext.getSymbolsTable().containsKey(this.word)){
-            processIdentifie(lexerContext);
+        Integer reservedWordToken = lexerContext.getReservedWordsTable().get(this.word);
 
+        if (reservedWordToken != null) {
+            lexerContext.getLexer().setNextToken(reservedWordToken);
+        } else {
+            this.truncateWordIfNecessary();
+
+            if (!this.isIdentifierInSymbolsTable()) this.addIdentifierToSymbolsTable();
+
+            this.emitIdentifier();
         }
     }
 
-    private void processIdentifie(LexerContext lexerContext){
-          if (!(this.word.length() <= 25)){
-             this.word = word.subSequence(0,24).toString();
-          }
-        lexerContext.getSymbolsTable().put("identifier",new SymbolTableEntry(this.word));
+    private void truncateWordIfNecessary () {
+        if (this.word.length() > 25){
+            String newWord = this.word.subSequence(0,24).toString();
+            this.lexerContext.getLogger().logWarning(
+                    "Identificador truncado ("+newWord+"): "+this.word
+            );
+            this.word = newWord;
+        }
+    }
 
+    private boolean isIdentifierInSymbolsTable () {
+        return this.lexerContext.getSymbolsTable().containsKey(this.word);
+    }
+
+    private void addIdentifierToSymbolsTable () {
+        SymbolTableEntry symbolTableEntry = new SymbolTableEntry();
+        symbolTableEntry.setLexeme(this.word);
+
+        this.lexerContext.getSymbolsTable().put(
+                this.word,
+                symbolTableEntry
+        );
+    }
+
+    private void emitIdentifier () {
+        this.lexerContext.getLexer().setNextToken(TokenNumbers.ID, this.word);
     }
 }
