ad418f3a5ea279fd58301db42df134d0c32169c0
Lucas Olivera <oliveralucasm@gmail.com>
2019-09-20 16:04:04 -0300

    Cosmetic changes

--- src/main/java/tpcompiladores/lexer/semanticActions/EmitSpecialCharacter.java
+++ src/main/java/tpcompiladores/lexer/semanticActions/EmitSpecialCharacter.java
@@ -3,10 +3,9 @@ package tpcompiladores.lexer.semanticActions;
 import tpcompiladores.lexer.LexerContext;
 
 public class EmitSpecialCharacter implements SemanticAction {
-
     @Override
     public void run(LexerContext lexerContext) {
         Character readCharacter = lexerContext.getCharactersReader().getLastReadCharacter();
-        lexerContext.getLexer().setNextToken((int)readCharacter);
+        lexerContext.getLexer().setNextToken((int) readCharacter);
     }
 }
