0be302258df9cfc863ee6bff74f84b43162ae5fb
Agustin Gerez <agustingerez2@gmail.com>
2019-09-26 18:02:04 -0300

    Class Reserved Words

---
+++ src/main/java/tpcompiladores/lexer/ReservedWords.java
@@ -0,0 +1,33 @@
+package tpcompiladores.lexer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReservedWords {
+    private static ReservedWords instance = new ReservedWords();
+    public static ReservedWords getInstance(){
+        return instance;
+    }
+
+    private Map<String, Integer> reservedWords = new HashMap<>();
+
+    private ReservedWords(){
+        this.reservedWords.put("if", TokenNumbers.IF);
+        this.reservedWords.put("else", TokenNumbers.ELSE);
+        this.reservedWords.put("end_if", TokenNumbers.END_IF);
+        this.reservedWords.put("print", TokenNumbers.PRINT);
+        this.reservedWords.put("int", TokenNumbers.INT);
+        this.reservedWords.put("begin", TokenNumbers.BEGIN);
+        this.reservedWords.put("end", TokenNumbers.END);
+        this.reservedWords.put("long", TokenNumbers.LONG);
+        this.reservedWords.put("do", TokenNumbers.DO);
+        this.reservedWords.put("while", TokenNumbers.WHILE);
+        this.reservedWords.put("class", TokenNumbers.CLASS);
+        this.reservedWords.put("extends", TokenNumbers.EXTENDS);
+
+    }
+
+    public Integer getTokenNumber(String lexeme){
+        return this.reservedWords.get(lexeme);
+    }
+}
--- src/main/java/tpcompiladores/lexer/TokenNumbers.java
+++ src/main/java/tpcompiladores/lexer/TokenNumbers.java
@@ -8,4 +8,16 @@ public class TokenNumbers {
     public static final int CONST_INT = 258;
     public static final int CONST_LONG = 259;
     public static final int CONST_STRING = 260;
+    public static final int IF = 261;
+    public static final int ELSE = 262;
+    public static final int END_IF = 263;
+    public static final int PRINT = 264;
+    public static final int INT = 265;
+    public static final int BEGIN = 266;
+    public static final int END = 267;
+    public static final int LONG = 268;
+    public static final int DO = 269;
+    public static final int WHILE = 270;
+    public static final int CLASS = 271;
+    public static final int EXTENDS = 272;
 }
