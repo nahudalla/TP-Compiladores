4ba1c8663da7c69e3ee3f6a68d0741ccc43f1552
Agustin Gerez <agustingerez2@gmail.com>
2019-11-05 17:47:07 -0300

    Class SyntacticTree/NumericConstantTree

--- src/main/java/tpcompiladores/syntacticTree/NumericConstantTree.java
+++ src/main/java/tpcompiladores/syntacticTree/NumericConstantTree.java
@@ -1,14 +1,19 @@
 package tpcompiladores.syntacticTree;
 
+import tpcompiladores.symbolsTable.SymbolsTableEntry;
 import tpcompiladores.symbolsTable.Type;
 
 public class NumericConstantTree extends SyntacticTree {
-    public NumericConstantTree(SyntacticTree leftTree, SyntacticTree rightTree) {
-        super(leftTree, rightTree);
+    private SymbolsTableEntry symbolsTableReference;
+
+    public NumericConstantTree(SymbolsTableEntry symbolsTableReference) {
+        super(null, null);
+
+        this.symbolsTableReference = symbolsTableReference;
     }
 
     @Override
     public Type resultType() {
-        return null;
+        return this.symbolsTableReference.getType();
     }
 }
