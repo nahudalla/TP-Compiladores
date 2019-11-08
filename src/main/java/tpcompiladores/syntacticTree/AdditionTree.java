e3a608f2074a3146ebf1e8f4d4ff98c9b9224899
Lucas Olivera <oliveralucasm@gmail.com>
2019-11-08 14:06:39 -0300

    Correction of matrices's inheritance

---
+++ src/main/java/tpcompiladores/syntacticTree/AdditionTree.java
@@ -0,0 +1,7 @@
+package tpcompiladores.syntacticTree;
+
+public class AdditionTree extends SmallResultTypeTree {
+    public AdditionTree(SyntacticTree leftTree, SyntacticTree rightTree) {
+        super(leftTree, rightTree);
+    }
+}
--- src/main/java/tpcompiladores/syntacticTree/ComparisonTree.java
+++ src/main/java/tpcompiladores/syntacticTree/ComparisonTree.java
@@ -2,13 +2,8 @@ package tpcompiladores.syntacticTree;
 
 import tpcompiladores.symbolsTable.Type;
 
-public class ComparisonTree extends SyntacticTree {
+public class ComparisonTree extends SmallResultTypeTree {
     public ComparisonTree(SyntacticTree leftTree, SyntacticTree rightTree) {
         super(leftTree, rightTree);
-    }
-
-    @Override
-    public Type resultType() {
-        return null;
     }
 }
